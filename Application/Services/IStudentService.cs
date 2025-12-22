aa10217e9517efb7c89a6af522e14b31d81fcdde
maggieeagle <oryol.margo@gmail.com>
2025-12-22 05:37:37 -0800

    fix

---
+++ Application/Services/IStudentService.cs
@@ -0,0 +1,13 @@
+using Domain.Entities;
+
+namespace ApplicationLayer.Services
+{
+    public interface IStudentService
+    {
+        Task<List<Student>> GetAllStudents();
+        Task<Student?> GetById(int id);
+        Task Add(Student student);
+        Task Update(Student student);
+        Task Delete(int id);
+    }
+}
